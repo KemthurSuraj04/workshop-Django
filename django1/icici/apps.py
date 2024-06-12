557999ad514025e845ab46e46924152b6b8e0f03
Kemthur Suraj <142688110+KemthurSuraj04@users.noreply.github.com>
2024-06-12 21:40:57 +0530

    Deleted icici directory

--- django1/icici/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class IciciConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'icici'
--- django1/icici/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from icici.models import Accounts
-class inputform(forms.ModelForm):
-    class Meta:
-        model=Accounts
-        fields=['firstname','lastname','aadhaar','phone']
